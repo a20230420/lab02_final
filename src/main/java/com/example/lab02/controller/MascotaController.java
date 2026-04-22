2c0df85996594ba2f65da43ad9fff9144d3614b3
Adrian <a20230420@pucp.edu.pe>
2026-04-22 17:18:17 -0500

    Pregunta 2, creado de listado y de botones de editar y eliminar

--- src/main/java/com/example/lab02/controller/MascotaController.java
+++ src/main/java/com/example/lab02/controller/MascotaController.java
@@ -1,14 +1,87 @@
 package com.example.lab02.controller;
 
+import com.example.lab02.dto.MascotaForm;
+import com.example.lab02.entity.Mascota;
 import com.example.lab02.repository.MascotaRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/mascota")
 public class MascotaController {
+
     final MascotaRepository mascotaRepository;
+
     public MascotaController(MascotaRepository mascotaRepository) {
         this.mascotaRepository = mascotaRepository;
+    }
+
+    @GetMapping("/list")
+    public String listaMascotas(Model model) {
+        model.addAttribute("mascotas", mascotaRepository.findAll());
+        return "listaMascotas";
+    }
+
+    public String validate(MascotaForm form) {
+        if(form.getNombre() == null || form.getNombre().isBlank()) return "El nombre es obligatorio";
+        if(form.getEspecie() == null || form.getEspecie().isBlank()) return "Indicar especie de la mascota";
+        if(form.getRaza() == null || form.getRaza().isBlank()) return "Indicar raza de la mascota";
+        if(form.getEdad() <= 0 || form.getEdad() >= 25) return "Indicar la edad de la mascota";
+        if(form.getNombreDueno() == null || form.getNombreDueno().isBlank()) return "El nombre de dueño es obligatorio";
+        if(form.getTelefono() == null || form.getTelefono().isBlank()) return "Indicar teléfono de contacto";
+        if (!form.getTelefono().matches("\\d+")) return "El teléfono solo debe contener números";
+        return null;
+    }
+
+    @GetMapping("/edit")
+    public String editMascota(@RequestParam Integer id, Model model) {
+        Mascota mascota = mascotaRepository.findById(id).orElse(null);
+        if(mascota == null) return "redirect:/mascota/list";
+
+        MascotaForm form = new MascotaForm();
+        form.setIdMascota(mascota.getIdMascota());
+        form.setNombre(mascota.getNombre());
+        form.setEspecie(mascota.getEspecie());
+        form.setRaza(mascota.getRaza());
+        form.setEdad(mascota.getEdad());
+        form.setNombreDueno(mascota.getNombreDueno());
+        form.setTelefono(mascota.getTelefono());
+        form.setEstado(mascota.getEstado());
+
+        model.addAttribute("mascotaForm", form);
+        model.addAttribute("mode","edit");
+        return "formMascota";
+    }
+
+    @PostMapping("/update")
+    public String updateMascota(@ModelAttribute MascotaForm form, Model model) {
+        String error = validate(form);
+        if (error != null) {
+            model.addAttribute("error", error);
+            model.addAttribute("mode","create");
+            return "formMascota";
+        }
+
+        Mascota mascota = mascotaRepository.findById(form.getIdMascota()).orElse(null);
+        if (mascota == null) return "mascota/list";
+        mascota.setNombre(form.getNombre());
+        mascota.setEspecie(form.getEspecie());
+        mascota.setRaza(form.getRaza());
+        mascota.setEdad(form.getEdad());
+        mascota.setNombreDueno(form.getNombreDueno());
+        mascota.setTelefono(form.getTelefono());
+        mascota.setEstado((form.getEstado()));
+
+        mascotaRepository.save(mascota);
+        return "redirect:/mascota/list";
+    }
+
+    @GetMapping("/delete")
+    public String deleteMascota(@RequestParam Integer id) {
+        if(mascotaRepository.existsById(id)){
+            mascotaRepository.deleteById(id);
+        }
+        return "redirect:/mascota/list";
     }
 }
--- src/main/java/com/example/lab02/dto/MascotaForm.java
+++ src/main/java/com/example/lab02/dto/MascotaForm.java
@@ -1,59 +1,38 @@
-package com.example.lab02.entity;
+package com.example.lab02.dto;
 
-import jakarta.persistence.*;
+public class MascotaForm {
 
-@Entity
-@Table(name = "mascota")
-public class Mascota {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
     private int idMascota;
-
     public int getIdMascota() {return idMascota;}
     public void setIdMascota(int idMascota) {this.idMascota = idMascota;}
 
-    @Column(name = "nombre")
     private String nombre;
-
     public String getNombre() {return nombre;}
     public void setNombre(String nombre) {this.nombre = nombre;}
 
-    @Column(name = "especie")
     private String especie;
-
     public String getEspecie() {return especie;}
     public void setEspecie(String especie) {this.especie = especie;}
 
-    @Column(name = "raza")
     private String raza;
-
     public String getRaza() {return raza;}
     public void setRaza(String raza) {this.raza = raza;}
 
-    @Column(name = "edad")
     private int edad;
-
     public int getEdad() {return edad;}
     public void setEdad(int edad) {this.edad = edad;}
 
-    @Column(name = "nombre_dueno")
     private String nombreDueno;
-
     public String getNombreDueno() {return nombreDueno;}
     public void setNombreDueno(String nombreDueno) {this.nombreDueno = nombreDueno;}
 
-    @Column(name = "telefono")
     private String telefono;
-
     public String getTelefono() {return telefono;}
     public void setTelefono(String telefono) {this.telefono = telefono;}
 
-    @Column(name = "estado")
     private int estado;
-
     public int getEstado() {return estado;}
     public void setEstado(int estado) {this.estado = estado;}
 
-    public Mascota(){}
+    public MascotaForm() {}
 }
